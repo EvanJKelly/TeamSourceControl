15076b45f7242c80bb2f7390b463ad0470086bd1
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-08-05 14:02:54 -0700

    Created add link form

--- TeamSourceControl/Form1.Designer.cs
+++ TeamSourceControl/Form1.Designer.cs
@@ -28,20 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LinkBox = new System.Windows.Forms.ComboBox();
+            this.LinkCboBox = new System.Windows.Forms.ComboBox();
             this.Links = new System.Windows.Forms.Label();
-            this.AddLInk = new System.Windows.Forms.Button();
-            this.UpdateLink = new System.Windows.Forms.Button();
-            this.DeleteLink = new System.Windows.Forms.Button();
+            this.MainAddLinkButton = new System.Windows.Forms.Button();
+            this.MainUpdateLinkButton = new System.Windows.Forms.Button();
+            this.MainDeleteLinkButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // LinkBox
+            // LinkCboBox
             // 
-            this.LinkBox.FormattingEnabled = true;
-            this.LinkBox.Location = new System.Drawing.Point(181, 66);
-            this.LinkBox.Name = "LinkBox";
-            this.LinkBox.Size = new System.Drawing.Size(210, 21);
-            this.LinkBox.TabIndex = 0;
+            this.LinkCboBox.FormattingEnabled = true;
+            this.LinkCboBox.Location = new System.Drawing.Point(181, 66);
+            this.LinkCboBox.Name = "LinkCboBox";
+            this.LinkCboBox.Size = new System.Drawing.Size(210, 21);
+            this.LinkCboBox.TabIndex = 0;
             // 
             // Links
             // 
@@ -52,43 +52,43 @@
             this.Links.TabIndex = 1;
             this.Links.Text = "Links:";
             // 
-            // AddLInk
+            // MainAddLinkButton
             // 
-            this.AddLInk.Location = new System.Drawing.Point(520, 50);
-            this.AddLInk.Name = "AddLInk";
-            this.AddLInk.Size = new System.Drawing.Size(108, 50);
-            this.AddLInk.TabIndex = 2;
-            this.AddLInk.Text = "Add Link";
-            this.AddLInk.UseVisualStyleBackColor = true;
+            this.MainAddLinkButton.Location = new System.Drawing.Point(520, 50);
+            this.MainAddLinkButton.Name = "MainAddLinkButton";
+            this.MainAddLinkButton.Size = new System.Drawing.Size(108, 50);
+            this.MainAddLinkButton.TabIndex = 2;
+            this.MainAddLinkButton.Text = "Add Link";
+            this.MainAddLinkButton.UseVisualStyleBackColor = true;
             // 
-            // UpdateLink
+            // MainUpdateLinkButton
             // 
-            this.UpdateLink.Location = new System.Drawing.Point(520, 147);
-            this.UpdateLink.Name = "UpdateLink";
-            this.UpdateLink.Size = new System.Drawing.Size(108, 54);
-            this.UpdateLink.TabIndex = 3;
-            this.UpdateLink.Text = "Update Link";
-            this.UpdateLink.UseVisualStyleBackColor = true;
+            this.MainUpdateLinkButton.Location = new System.Drawing.Point(520, 147);
+            this.MainUpdateLinkButton.Name = "MainUpdateLinkButton";
+            this.MainUpdateLinkButton.Size = new System.Drawing.Size(108, 54);
+            this.MainUpdateLinkButton.TabIndex = 3;
+            this.MainUpdateLinkButton.Text = "Update Link";
+            this.MainUpdateLinkButton.UseVisualStyleBackColor = true;
             // 
-            // DeleteLink
+            // MainDeleteLinkButton
             // 
-            this.DeleteLink.Location = new System.Drawing.Point(520, 243);
-            this.DeleteLink.Name = "DeleteLink";
-            this.DeleteLink.Size = new System.Drawing.Size(108, 52);
-            this.DeleteLink.TabIndex = 4;
-            this.DeleteLink.Text = "Delete Link";
-            this.DeleteLink.UseVisualStyleBackColor = true;
+            this.MainDeleteLinkButton.Location = new System.Drawing.Point(520, 243);
+            this.MainDeleteLinkButton.Name = "MainDeleteLinkButton";
+            this.MainDeleteLinkButton.Size = new System.Drawing.Size(108, 52);
+            this.MainDeleteLinkButton.TabIndex = 4;
+            this.MainDeleteLinkButton.Text = "Delete Link";
+            this.MainDeleteLinkButton.UseVisualStyleBackColor = true;
             // 
             // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.DeleteLink);
-            this.Controls.Add(this.UpdateLink);
-            this.Controls.Add(this.AddLInk);
+            this.Controls.Add(this.MainDeleteLinkButton);
+            this.Controls.Add(this.MainUpdateLinkButton);
+            this.Controls.Add(this.MainAddLinkButton);
             this.Controls.Add(this.Links);
-            this.Controls.Add(this.LinkBox);
+            this.Controls.Add(this.LinkCboBox);
             this.Name = "MainPage";
             this.Text = "MainPage";
             this.ResumeLayout(false);
@@ -98,11 +98,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox LinkBox;
+        private System.Windows.Forms.ComboBox LinkCboBox;
         private System.Windows.Forms.Label Links;
-        private System.Windows.Forms.Button AddLInk;
-        private System.Windows.Forms.Button UpdateLink;
-        private System.Windows.Forms.Button DeleteLink;
+        private System.Windows.Forms.Button MainAddLinkButton;
+        private System.Windows.Forms.Button MainUpdateLinkButton;
+        private System.Windows.Forms.Button MainDeleteLinkButton;
     }
 }
 
