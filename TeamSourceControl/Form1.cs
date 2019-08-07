f91b439e1fefa5726dadc48611d4660624c8bad0
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-08-07 15:13:06 -0700

    Created add link class

--- TeamSourceControl/Form1.cs
+++ TeamSourceControl/Form1.cs
@@ -12,9 +12,24 @@ namespace TeamSourceControl
 {
     public partial class MainPage : Form
     {
+
         public MainPage()
         {
+            
             InitializeComponent();
+        }
+
+        private void MainDeleteLinkButton_Click(object sender, EventArgs e)
+        {
+            if(LinkCboBox.SelectedIndex >= 0)
+            {
+                LinkCboBox.Items.RemoveAt(LinkCboBox.SelectedIndex);
+            }
+        }
+
+        private void MainAddLinkButton_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
---
+++ TeamSourceControl/Link.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSourceControl
+{
+   public class Link
+    {
+        public Link()
+        {
+
+        }
+
+        public Link( string URL, string Title)
+        {
+          
+            LinkURL = URL;
+            LinkTitle = Title;
+        }
+
+        public   int LinkID { get; set; }
+
+        public string LinkURL { get; set; }
+
+        public string LinkTitle { get; set; }
+
+
+
+
+    }
+}
