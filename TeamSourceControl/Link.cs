7d2925f0e98432191bde591c2a6271d740e68c91
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-08-07 15:19:57 -0700

    Created Link class

---
+++ TeamSourceControl/Link.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamSourceControl
+{
+    public class Link
+    {
+        public Link()
+        {
+
+        }
+
+
+        public Link(String Title, String URL)
+        {
+
+
+            LinkTitle = Title;
+            LinkURL = URL;
+        }
+
+
+        public int LinkId { get; set; }
+
+        public string LinkTitle { get; set; }
+
+        public string LinkURL { get; set; }
+
+
+       
+    }
+}
