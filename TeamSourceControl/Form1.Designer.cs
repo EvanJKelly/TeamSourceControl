57a9ece43d5c1466658b1d315300c3017ebf1922
robert lowry <lowryrob2001@gmail.com>
2019-08-21 15:47:13 -0700

    Added fuctionality to search bar

--- TeamSourceControl/Form1.Designer.cs
+++ TeamSourceControl/Form1.Designer.cs
@@ -109,6 +109,7 @@
             this.btnSearch.TabIndex = 8;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // MainPage
             // 
